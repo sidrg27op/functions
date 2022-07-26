38b6d3bb77c586e0e00f08a2f97bf914d774ca6e
siddharth raghav <thakurraghav27@gmail.com>
2022-07-26 18:00:33 +0530

    default parameters

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,13 @@
 'use strict';
+// default parameters
+
+const flight = function (f = 12, s = 3, p = 2) {
+  const booking = {
+    f,
+    s,
+    p,
+  };
+  console.log(booking);
+};
+
+flight(234, undefined, 54);
