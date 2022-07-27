77064607cf097ad9c8527f0c9965bbba86ead71a
siddharth raghav <thakurraghav27@gmail.com>
2022-07-27 13:33:43 +0530

    call and apply

--- starter/script.js
+++ starter/script.js
@@ -11,3 +11,38 @@ const flight = function (f = 12, s = 3, p = 2) {
 };
 
 flight(234, undefined, 54);
+
+const greet = greeting => name => console.log(`${greeting} ${name}`);
+
+const op = greet('op');
+op('nuyni');
+
+const nuni = {
+  nam: 'nuni',
+  itacode: 'ew',
+  booking: [],
+
+  book(code, bname) {
+    this.booking.push(`${code},${bname}`);
+  },
+};
+
+nuni.book('32', 'ew');
+console.log(nuni.booking);
+
+const buni = {
+  nam: 'ooo',
+  itacode: 'sa',
+  booking: [],
+};
+
+const nok = nuni.book;
+
+nok.call(buni, 'ds', 'eweweew');
+nok.call(nuni, 'googogog', 'opoop');
+console.log(nuni);
+console.log(buni.booking);
+
+// const [f, n] = nuni.booking;
+// const [o, p] = (f.split(','), n.split(','));
+// console.log(o, p);
